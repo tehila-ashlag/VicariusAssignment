879904476776864bc1db50b0aa9f4ed0a4eb046b
tehila-ashlag <tehila82696@gmail.com>
2023-11-22 21:11:00 +0200

    init commit

---
+++ homeassignment/homeassignment/src/main/java/com/vicarius/homeassignment/HomeassignmentApplication.java
@@ -0,0 +1,13 @@
+package com.vicarius.homeassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeassignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeassignmentApplication.class, args);
+	}
+
+}
