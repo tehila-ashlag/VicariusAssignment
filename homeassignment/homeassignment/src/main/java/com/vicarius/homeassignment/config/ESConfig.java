a9b25c12e169d02ca79a36f9ab1e78f1be01afe0
tehila-ashlag <tehila82696@gmail.com>
2023-11-26 02:17:36 +0200

    commenting the explicitly declaring of singleton scope

--- homeassignment/homeassignment/src/main/java/com/vicarius/homeassignment/config/ESConfig.java
+++ homeassignment/homeassignment/src/main/java/com/vicarius/homeassignment/config/ESConfig.java
@@ -10,7 +10,7 @@ import org.apache.http.message.BasicHeader;
 import org.elasticsearch.client.RestClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
+//import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class ESConfig {
